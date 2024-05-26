5186cc50689ad6bfd7372de9b6f7843dc6b0dfaa
cayan701 <cayan701@gmail.com>
2024-05-27 04:15:59 +0530

    Add new files

--- ev-car/src/Components/Background/Background.jsx
+++ ev-car/src/Components/Background/Background.jsx
@@ -1,6 +1,11 @@
 import './Background.css';
+import video1 from '../../assets/video1.mp4';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
 
-const Background = () => {
+
+const Background = ({ heroCount, playStatus }) => {
     return (
         <div>
             
--- ev-car/src/Components/Hero/Hero.jsx
+++ ev-car/src/Components/Hero/Hero.jsx
@@ -1,6 +1,12 @@
 import './Hero.css';
 
 const Hero = () => {
+    let heroData = [
+        { text1: 'Dive into', text2: 'What you love'},
+        { text1: 'Indulge', text2: 'Your Passions'},
+        { text1: 'Give in to', text2: 'Everything'}
+    ]
+
     return (
         <div>
 
