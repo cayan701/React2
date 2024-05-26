ae9740b2d7b0cb532c518d5d32937a01ed4f6cf8
cayan701 <cayan701@gmail.com>
2024-05-27 04:37:59 +0530

    Add new files

--- ev-car/src/App.jsx
+++ ev-car/src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
+import Background from "./Components/Background/Background";
+
 const App = () => {
+  let heroData = [
+    { text1: 'Dive into', text2: 'What you love'},
+    { text1: 'Indulge', text2: 'Your Passions'},
+    { text1: 'Give in to', text2: 'Everything'}
+  ]
+
+  const [ heroCount, setHeroCount ] = useState(2);
+  const [ playStatus, setPlayStatus ] = useState(true);
+
   return (
     <div>
-
+        <Background playStatus={playStatus} heroCount={heroCount} />
     </div>
-  );
+  )
 }
 
 export default App;
--- ev-car/src/Components/Background/Background.jsx
+++ ev-car/src/Components/Background/Background.jsx
@@ -7,9 +7,23 @@ import image3 from '../../assets/image3.png';
 
 const Background = ({ heroCount, playStatus }) => {
     if(playStatus) {
-        <video className='background' autoPlay loop muted>
-            <source src={video1} type='video/mp4' />
-        </video>
+        return (
+            <video className='background' autoPlay loop muted>
+                <source src={video1} type='video/mp4' />
+            </video>
+        );
+    } else if(heroCount === 0) {
+        return (
+            <img src={image1} alt="" className='background'/>
+        );
+    } else if(heroCount === 1) {
+        return (
+            <img src={image2} alt="" className='background'/>
+        );
+    } else if(heroCount === 2) {
+        return (
+            <img src={image3} alt="" className='background'/>
+        );
     }
 }
 
