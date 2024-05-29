58100eede34c31d4dd2e546bdcd5dc9eccf28ddc
cayan701 <cayan701@gmail.com>
2024-05-29 06:46:06 +0530

    Add new files

--- ev-car/src/Components/Navbar/Navbar.jsx
+++ ev-car/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='nav' >
-            <h2 className="nav-logo">Rivion</h2>
+            <div className="nav-logo">Rivion</div>
             <ul className="nav-menu">
                 <li>Home</li>
                 <li>Our Products</li>
