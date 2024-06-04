6fb4c929ff6724b14e26cda11917e58b0f617823
cayan701 <cayan701@gmail.com>
2024-06-04 05:45:03 +0530

    Add new files

--- ev-car/src/App.jsx
+++ ev-car/src/App.jsx
@@ -1,4 +1,12 @@
 const App = () => {
+  let heroData = [
+    {text1: 'Dive into', text2: 'what you love!'},
+    {text1: 'Create your', text2: 'own destiny!'},
+    {text1: 'Be Cool', text2: 'Be the best!'},
+  ];
+
+  
+
   return (
     <div>
 
