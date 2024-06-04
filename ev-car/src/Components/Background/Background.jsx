36d3bfbbad1f4fb9d51046471efb0c82669e4d3e
cayan701 <cayan701@gmail.com>
2024-06-05 03:47:22 +0530

    Add new files

--- ev-car/src/Components/Background/Background.jsx
+++ ev-car/src/Components/Background/Background.jsx
@@ -1,13 +1,16 @@
 import './Background.css';
 
-
+import video1 from '../../assets/video1.mp4';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
 
 const Background = ({ heroCount, playVideo }) => {
 
     if(playVideo) {
         return (
-            <video className='background'  >
-                <source src={} />
+            <video className='background' autoPlay loop muted >
+                <source src={video1}  />
             </video>
         )
     }
