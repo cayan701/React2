d2881dc1b32d6099a3a1eb391ea2f91eca58f5cd
cayan701 <cayan701@gmail.com>
2024-06-05 03:47:32 +0530

    Add new files

--- ev-car/src/App.jsx
+++ ev-car/src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+import Background from "./Components/Background/Background";
+
 const App = () => {
+
   let heroData = [
     {text1: 'Dive into', text2: 'what you love!'},
     {text1: 'Create your', text2: 'own destiny!'},
     {text1: 'Be Cool', text2: 'Be the best!'},
   ];
 
-  
+  const [ heroCount, setHeroCount ] = useState(2);
+  const [ playVideo, setPlayVideo ] = useState(false);
 
   return (
     <div>
-
+      <Background heroCount={heroCount} playVideo={playVideo} />
     </div>
   );
 }
