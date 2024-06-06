8d79ce3ee388531ffcb924cff3ed50184b22d7b0
cayan701 <cayan701@gmail.com>
2024-06-06 06:15:14 +0530

    Add new files

--- ev-car/src/Components/Hero/Hero.jsx
+++ ev-car/src/Components/Hero/Hero.jsx
@@ -6,6 +6,18 @@ import pause_icon from '../../assets/pause_icon.png';
 
 
 const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) => {
+    let a = () => {
+        setHeroCount(0);
+    }
+
+    let b = () => {
+        setHeroCount(1);
+    }
+
+    let c = () => {
+        setHeroCount(2);
+    }
+    
     return (
         <div className='hero'>
             <div className='hero-text'>
@@ -18,9 +30,9 @@ const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) 
             </div>
             <div className='hero-dot-play'>
                 <ul className='hero-dots'>
-                    <li className={heroCount === 0 ? 'hero-dot orange' : 'hero-dot'}></li>
-                    <li className={heroCount === 1 ? 'hero-dot orange' : 'hero-dot'}></li>
-                    <li className={heroCount === 2 ? 'hero-dot orange' : 'hero-dot'}></li>
+                    <li onClick={() => a()} className={heroCount === 0 ? 'hero-dot orange' : 'hero-dot'}></li>
+                    <li onClick={() => b()} className={heroCount === 1 ? 'hero-dot orange' : 'hero-dot'}></li>
+                    <li onClick={() => c()} className={heroCount === 2 ? 'hero-dot orange' : 'hero-dot'}></li>
                 </ul>
             </div>
         </div>
